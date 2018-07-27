d64a3d11bc2b50b16a08d9834d5396e6366d1f87
omitent <mickl0102@outlook.com>
2018-07-27 11:54:11 -0500

    commiting working react component

---
+++ app/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Layout extends React.Component {
+	render() {
+		return(
+			<div id='Layout'>
+				Hello from React!
+			</div>
+		);
+	}
+}
+
+export default Layout;
---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './components/Layout';
+import './style.css';
+
+ReactDOM.render(<Layout />, document.getElementById('app'));
